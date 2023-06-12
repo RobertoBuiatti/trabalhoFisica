9658758c818ea7ba5772ec93390baab2bd39b0d6
Roberto Caetano Buiatti <102183235+RobertoBuiatti@users.noreply.github.com>
2023-06-11 23:58:03 -0300

    todas contas prontas

--- calculos/exemplo5/exemplo5.js
+++ calculos/exemplo5/exemplo5.js
@@ -1,10 +1,10 @@
 function calcular() {
-    var massaInput = document.getElementById("Massa");
-    var forcaInput = document.getElementById("forca");
-    var razaoInput = document.getElementById("Razao");
-    var qntBlocosInput = document.getElementById("qntBlocos");
+    var massaInput = parseFloat(document.getElementById("Massa").value);
+    var forcaInput = parseFloat(document.getElementById("Forca").value);
+    var razaoInput = parseFloat(document.getElementById("Razao").value);
+    var qntBlocosInput = parseFloat(document.getElementById("qntBlocos").value);
     // var coeficienteAtritoInput = document.getElementById("coeficienteAtrito");
-    var tracaoInput = document.getElementById("TracaoN");
+    var tracaoInput = parseFloat(document.getElementById("TracaoN").value);
 
 
 
@@ -15,15 +15,20 @@ function calcular() {
     var massaDaTensao = tracaoInput * (massaTensaoN + massaInput) / 2; // massa total dos blocos da tensao
 
     var aceleracao = forcaInput / massaTotal;
-
-    var tensao = massaDaTensao * aceleracao; // tensao no bloco n
-
-console.log(forcaInput);
-console.log(massaDaTensao);
-console.log(massaTotal);
+    var tensao = NaN;
+    
+    if(tracaoInput < qntBlocosInput){
+    tensao = massaDaTensao * aceleracao; // tensao no bloco n
+    }
+    else if(tracaoInput == qntBlocosInput){
+        tensao = forcaInput;
+    }
+    console.log(forcaInput);
+    console.log(massaDaTensao);
+    console.log(massaTotal);
 
     var resultado = document.getElementById("resultado");
-    resultado.innerHTML = "<br>Força de aceleração: " + aceleracao.toFixed(2) + " m/s²<br>Força de tensao do bloco " + tracaoInput.toFixed(2) + ": " + tensao.toFixed(2) + " N";
+    resultado.innerHTML = "<br>Força de aceleração: " + aceleracao.toFixed(2) + " m/s²<br>Força de tensão no Bloco escolhido: " + tensao.toFixed(2) + " N";
 
 }
 
