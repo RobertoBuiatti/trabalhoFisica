77fc66ed4f5933bc5daae68cfc6552f7f79293df
Eduado Machado Rezende <eduardomachadorezende0@gmail.com>
2023-06-17 00:53:03 -0300

    Refatora codigo

--- calculos/exemplo1/exemplo1.js
+++ calculos/exemplo1/exemplo1.js
@@ -10,39 +10,47 @@ function calcular() {
     var gravidade = parseFloat(document.getElementById("gravidade").value);
     var tempo = parseFloat(document.getElementById("Tempo").value);
 
+    if (isNaN(massa1) || isNaN(massa2) || isNaN(massa3) || isNaN(coeficiente1) || isNaN(coeficiente2) || isNaN(coeficiente3) || isNaN(forca) || isNaN(gravidade)) {
+        var resultado = document.getElementById("resultado");
+        resultado.innerHTML = "Preencha os campos das massas, dos coeficientes de atrito, da força e da gravidade com valores numéricos.";
+        return;
+    }
+
     var forcaAtrito1 = coeficiente1 * massa1 * gravidade;
     var forcaAtrito2 = coeficiente2 * massa2 * gravidade;
     var forcaAtrito3 = coeficiente3 * massa3 * gravidade;
     var forcaAtritoTotal = forcaAtrito1 + forcaAtrito2 + forcaAtrito3;
 
-    if (!isNaN(massa1) && !isNaN(massa2) && !isNaN(massa3) && !isNaN(coeficiente1) && !isNaN(coeficiente2) && !isNaN(coeficiente3) && !isNaN(gravidade)) {
-    // Calcular a aceleração
-    if (forca >= forcaAtritoTotal) {
-        var aceleracao = ((forca) - (forcaAtritoTotal)) / (massa1 + massa2 + massa3);
-        var tensao1 = (massa1 * aceleracao) + (forcaAtrito1);
-        var tensao2 = (massa2 * aceleracao) + (forcaAtrito2) + tensao1;
-        //var tensao3 = (massa3 * aceleracao) + (forcaAtrito3)+tensao2;
-        var velocidadeFinal = aceleracao * tempo;
-        var deslocamento = (aceleracao * (tempo * tempo)) / 2;
-        if (!isNaN(tempo)) {
-            var resultado = document.getElementById("resultado");
-            resultado.innerHTML = "Aceleração: " + aceleracao.toFixed(2) + " m/s²<br>Tensão entre 1 e 2: " + tensao1.toFixed(2) + " N<br>Tensão entre 2 e 3: " + tensao2.toFixed(2) + " N<br>Força de atrito Corpo 1: " + forcaAtrito1.toFixed(2) + " N<br>Força de atrito Corpo 2: " + forcaAtrito2.toFixed(2) + " N<br>Força de atrito Corpo 3: " + forcaAtrito3.toFixed(2) + " N<br>Valor mínimo para força aplicada para puxar o fio: " + forcaAtritoTotal.toFixed(2) + " N<br>Velocidade ao final do tempo passado: " + velocidadeFinal.toFixed(2) + " m/s<br>Deslocamento ao final do tempo passado: " + deslocamento.toFixed(2) + " m";
-        }
-        else {
-            var resultado = document.getElementById("resultado");
-            resultado.innerHTML = "Aceleração: " + aceleracao.toFixed(2) + " m/s²<br>Tensão entre 1 e 2: " + tensao1.toFixed(2) + " N<br>Tensão entre 2 e 3: " + tensao2.toFixed(2) + " N<br>Força de atrito Corpo 1: " + forcaAtrito1.toFixed(2) + " N<br>Força de atrito Corpo 2: " + forcaAtrito2.toFixed(2) + " N<br>Força de atrito Corpo 3: " + forcaAtrito3.toFixed(2) + " N<br>Valor mínimo para força aplicada para puxar o fio: " + forcaAtritoTotal.toFixed(2) + " N<br>Para saber a velocidade e o deslocamento, insira um valor para o tempo.";
-        }
-
-    } else {
+    if (forca < forcaAtritoTotal) {
         var resultado = document.getElementById("resultado");
-        resultado.innerHTML = "A força é menor que a força de atrito total, logo o movimento é impossivel.<br>Valor mínimo para força aplicada para puxar o fio: " + forcaAtritoTotal.toFixed(2) + " N";
-
+        resultado.innerHTML = "A força é menor que a força de atrito total, logo o movimento é impossível.<br>Valor mínimo para força aplicada para puxar o fio: " + forcaAtritoTotal.toFixed(2) + " N";
+        return;
     }
 
+    // Calcular a aceleração
+    var aceleracao = (forca - forcaAtritoTotal) / (massa1 + massa2 + massa3);
+    var tensao1 = massa1 * aceleracao + forcaAtrito1;
+    var tensao2 = massa2 * aceleracao + forcaAtrito2 + tensao1;
+    var velocidadeFinal = aceleracao * tempo;
+    var deslocamento = (aceleracao * tempo * tempo) / 2;
 
-}
-else {
-    var resultado = document.getElementById("resultado");
-    resultado.innerHTML = "Preencha os campos das massas, dos coeficiêntes de atrito, da força e da gravidade com valores numéricos.";
-}
+    if (isNaN(tempo)) {
+        var resultado = document.getElementById("resultado");
+        resultado.innerHTML = "Aceleração: " + aceleracao.toFixed(2) + " m/s²<br>Tensão entre 1 e 2: " +
+            tensao1.toFixed(2) + " N<br>Tensão entre 2 e 3: " + tensao2.toFixed(2) +
+            " N<br>Força de atrito Corpo 1: " + forcaAtrito1.toFixed(2) + " N<br>Força de atrito Corpo 2: " +
+            "" + forcaAtrito2.toFixed(2) + " N<br>Força de atrito Corpo 3: " + forcaAtrito3.toFixed(2) +
+            " N<br>Valor mínimo para força aplicada para puxar o fio: " + forcaAtritoTotal.toFixed(2) +
+            " N<br>Para saber a velocidade e o deslocamento, insira um valor para o tempo.";
+    } else {
+        var resultado = document.getElementById("resultado");
+        resultado.innerHTML = "Aceleração: " + aceleracao.toFixed(2) + " m/s²<br>Tensão entre 1 e 2: " +
+            tensao1.toFixed(2) + " N<br>Tensão entre 2 e 3: " + tensao2.toFixed(2) +
+            " N<br>Força de atrito Corpo 1: " + forcaAtrito1.toFixed(2) + " N<br>Força de atrito Corpo 2: " +
+            "" + forcaAtrito2.toFixed(2) + " N<br>Força de atrito Corpo 3: " +
+            forcaAtrito3.toFixed(2) + " N<br>Valor mínimo para força aplicada para puxar o fio: " +
+            forcaAtritoTotal.toFixed(2) + " N<br>Velocidade ao final do tempo passado: " +
+            velocidadeFinal.toFixed(2) + " m/s<br>Deslocamento ao final do tempo passado: " +
+            deslocamento.toFixed(2) + " m";
+    }
 }
--- calculos/exemplo2/exemplo2.js
+++ calculos/exemplo2/exemplo2.js
@@ -4,11 +4,19 @@ function calcular() {
     var massa2 = parseFloat(document.getElementById("massa2").value);
     var gravidade = parseFloat(document.getElementById("gravidade").value);
     var tempo = parseFloat(document.getElementById("Tempo").value);
-    
 
-    if (!isNaN(massa1) && !isNaN(massa2) && !isNaN(gravidade)) {
+    if (isNaN(massa1) || isNaN(massa2) || isNaN(gravidade)) {
+        var resultado = document.getElementById("resultado");
+        resultado.innerHTML = "Preencha os campos das massas e da gravidade com valores numéricos!";
+        return;
+    }
+
+    if (isNaN(tempo)) {
+        tempo = 0; // Definir tempo como zero se não for fornecido
+    }
+
     // Calcular a aceleração
-    var aceleracao = massa2 * gravidade / (massa1 + massa2);
+    var aceleracao = Math.abs(massa2 * gravidade / (massa1 + massa2));
 
     // Calcular a tensão do fio
     var tensao = massa1 * aceleracao;
@@ -16,17 +24,10 @@ function calcular() {
     var deslocamento = (aceleracao * (tempo * tempo)) / 2;
 
     // Exibir o resultado
-    if (!isNaN(tempo)){
     var resultado = document.getElementById("resultado");
-    resultado.innerHTML = "Aceleração no sentido do bloco suspenso: " + aceleracao.toFixed(2) + " m/s²<br>Tensão do Fio: " + tensao.toFixed(2) + " N<br>Velocidade ao final do tempo passado: " + velocidadeFinal.toFixed(2) + " m/s<br>Deslocamento ao final do tempo passado: " + deslocamento.toFixed(2) + " m";
-}
-else {
-    var resultado = document.getElementById("resultado");
-    resultado.innerHTML = "Aceleração no sentido do bloco suspenso: " + aceleracao.toFixed(2) + " m/s²<br>Tensão do Fio: " + tensao.toFixed(2) + " N";    
-}
-}
-else {
-    var resultado = document.getElementById("resultado");
-    resultado.innerHTML = "Preencha ao menos os campos das massas e da gravidade!";
-}
+    resultado.innerHTML = "Aceleração no sentido do bloco suspenso: " + aceleracao.toFixed(2) + " m/s²<br>Tensão do Fio: " + tensao.toFixed(2) + " N";
+
+    if (!isNaN(tempo)) {
+        resultado.innerHTML += "<br>Velocidade ao final do tempo passado: " + velocidadeFinal.toFixed(2) + " m/s<br>Deslocamento ao final do tempo passado: " + deslocamento.toFixed(2) + " m";
+    }
 }
