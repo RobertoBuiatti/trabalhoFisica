99602cefed4a5c86a362bc55fe6574f48acee883
Roberto Caetano Buiatti <102183235+RobertoBuiatti@users.noreply.github.com>
2023-07-01 23:47:23 -0300

    correção exercicio4

--- calculos/exemplo4/exemplo4.js
+++ calculos/exemplo4/exemplo4.js
@@ -23,31 +23,41 @@ function calcular() {
             
             if (!isNaN(tempo)) {
                 var resultado = document.getElementById("resultado");
-                resultado.innerHTML = "Aceleração no sentido do peso no plano inclinado: " + aceleracao.toFixed(2) + " m/s²<br>Tensão do Fio: " + tensao.toFixed(2) + " N<br>Velocidade Final: " + velocidadeFinal.toFixed(2) + " m/s<br>Deslocamento: " + deslocamento.toFixed(2) + " m<br>Força de atrito no bloco sobre o plano: " + forcaAtrito1.toFixed(2) + " N";
+                resultado.innerHTML = "Aceleração no sentido do peso no plano inclinado: " + aceleracao.toFixed(4) + " m/s²<br>Tensão do Fio: " + tensao.toFixed(4) + " N<br>Velocidade Final: " + velocidadeFinal.toFixed(4) + " m/s<br>Deslocamento: " + deslocamento.toFixed(4) + " m<br>Força de atrito no bloco sobre o plano: " + forcaAtrito1.toFixed(4) + " N<br>Força peso tangente: " + pesoTangente.toFixed(4) + " N";
             }
             else {
                 var resultado = document.getElementById("resultado");
-                resultado.innerHTML = "Aceleração no sentido do peso no plano inclinado: " + aceleracao.toFixed(2) + " m/s²<br>Tensão do Fio: " + tensao.toFixed(2) + " N<br>Força de atrito no bloco sobre o plano: " + forcaAtrito1.toFixed(2) + " N";
+                resultado.innerHTML = "Aceleração no sentido do peso no plano inclinado: " + aceleracao.toFixed(4) + " m/s²<br>Tensão do Fio: " + tensao.toFixed(4) + " N<br>Força de atrito no bloco sobre o plano: " + forcaAtrito1.toFixed(4) + " N<br>Força peso tangente: " + pesoTangente.toFixed(4) + " N";
             }
         }
         else if (pesoTangente == ((massa2 * gravidade)+forcaAtrito1)) {
             var tensao = (massa2 * gravidade)+forcaAtrito1;
             var aceleracao = 0;
             var resultado = document.getElementById("resultado");
-            resultado.innerHTML = "Aceleração: " + aceleracao.toFixed(2) + " m/s²<br>Tensão do Fio: " + tensao.toFixed(2) + " N<br>Força de atrito no bloco sobre o plano: " + forcaAtrito1.toFixed(2) + " N";
+            resultado.innerHTML = "Aceleração: " + aceleracao.toFixed(4) + " m/s²<br>Tensão do Fio: " + tensao.toFixed(4) + " N<br>Força de atrito no bloco sobre o plano: " + forcaAtrito1.toFixed(4) + " N<br>Força peso tangente: " + pesoTangente.toFixed(4) + " N";
         }
         else {
-            var aceleracao = ((massa2 * gravidade) - ((pesoTangente)+forcaAtrito1)) / (massa1 + massa2);
+            if ((pesoTangente + forcaAtrito1) > (massa2 * gravidade)) {
+                aceleracao = 0;
+            }
+            else {
+            var aceleracao = ((massa2 * gravidade) - pesoTangente-forcaAtrito1) / (massa1 + massa2);
+            // p2 - pesoTangente - forcaAtrito1 = aceleracao * (massa1 + massa2)
+        }
             var tensao = (massa2 * gravidade) - (massa2 * aceleracao);
             var velocidadeFinal = aceleracao * tempo;
             var deslocamento = (aceleracao * (tempo * tempo)) / 2;
-            if (!isNaN(tempo)) {
+            if (!isNaN(tempo) && aceleracao!=0) {
                 var resultado = document.getElementById("resultado");
-                resultado.innerHTML = "Aceleração no sentido do peso suspenso: " + aceleracao.toFixed(2) + " m/s²<br>Tensão do Fio: " + tensao.toFixed(2) + " N<br>Velocidade Final: " + velocidadeFinal.toFixed(2) + " m/s<br>Deslocamento: " + deslocamento.toFixed(2) + " m<br>Força de atrito no bloco sobre o plano: " + forcaAtrito1.toFixed(2) + " N";
+                resultado.innerHTML = "Aceleração no sentido do peso suspenso: " + aceleracao.toFixed(4) + " m/s²<br>Tensão do Fio: " + tensao.toFixed(4) + " N<br>Velocidade Final: " + velocidadeFinal.toFixed(4) + " m/s<br>Deslocamento: " + deslocamento.toFixed(2) + " m<br>Força de atrito no bloco sobre o plano: " + forcaAtrito1.toFixed(2) + " N<br>Força peso tangente: " + pesoTangente.toFixed(2) + " N";
+            }
+            else if (aceleracao!=0){
+                var resultado = document.getElementById("resultado");
+                resultado.innerHTML = "Aceleração no sentido do peso suspenso: " + aceleracao.toFixed(4) + " m/s²<br>Tensão do Fio: " + tensao.toFixed(4) + " N<br>Força de atrito no bloco sobre o plano: " + forcaAtrito1.toFixed(4) + " N<br>Força peso tangente: " + pesoTangente.toFixed(4) + " N";
             }
             else {
                 var resultado = document.getElementById("resultado");
-                resultado.innerHTML = "Aceleração no sentido do peso suspenso: " + aceleracao.toFixed(2) + " m/s²<br>Tensão do Fio: " + tensao.toFixed(2) + " N<br>Força de atrito no bloco sobre o plano: " + forcaAtrito1.toFixed(2) + " N";
+                resultado.innerHTML = "Aceleração: " + aceleracao.toFixed(4) + " m/s²<br>Tensão do Fio: " + tensao.toFixed(4) + " N<br>Força de atrito no bloco sobre o plano: " + forcaAtrito1.toFixed(4) + " N<br>Força peso tangente: " + pesoTangente.toFixed(4) + " N";
             }
         }
     }
