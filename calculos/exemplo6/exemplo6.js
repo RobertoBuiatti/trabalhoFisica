482c0bc9d7ec42ec625bddd7111601473b171c83
Roberto Caetano Buiatti <102183235+RobertoBuiatti@users.noreply.github.com>
2023-07-02 00:11:53 -0300

    correçao exercicio6

--- calculos/exemplo6/exemplo6.js
+++ calculos/exemplo6/exemplo6.js
@@ -4,16 +4,14 @@ function calcular() {
     var massa2 = parseFloat(document.getElementById("massa2").value);
     var massa3 = parseFloat(document.getElementById("massa3").value);
     var coeficiente1 = parseFloat(document.getElementById("coeficienteAtrito1").value);
-    var coeficiente2 = parseFloat(document.getElementById("coeficienteAtrito2").value);
-    var coeficiente3 = parseFloat(document.getElementById("coeficienteAtrito3").value);
     var forca = parseFloat(document.getElementById("Forca").value);
     var gravidade = parseFloat(document.getElementById("gravidade").value);
     var tempo = parseFloat(document.getElementById("Tempo").value);
 
-    if (!isNaN(massa1) && !isNaN(massa2) && !isNaN(massa3) && !isNaN(coeficiente1) && !isNaN(coeficiente2) && !isNaN(coeficiente3) && !isNaN(forca) && !isNaN(gravidade)) {
+    if (!isNaN(massa1) && !isNaN(massa2) && !isNaN(massa3) && !isNaN(coeficiente1) && !isNaN(forca) && !isNaN(gravidade)) {
         var forcaAtrito1 = coeficiente1 * massa1 * gravidade;
-        var forcaAtrito2 = coeficiente2 * massa2 * gravidade;
-        var forcaAtrito3 = coeficiente3 * massa3 * gravidade;
+        var forcaAtrito2 = coeficiente1 * massa2 * gravidade;
+        var forcaAtrito3 = coeficiente1 * massa3 * gravidade;
         var forcaAtritoTotal = forcaAtrito1 + forcaAtrito2 + forcaAtrito3;
 
 
@@ -22,10 +20,11 @@ function calcular() {
             var aceleracao = (forca - forcaAtritoTotal) / (massa1 + massa2 + massa3);
 
             var forcaBA = ((forca) - (coeficiente1 * massa1 * gravidade) - (massa1 * aceleracao));
-            var forcaBC = ((massa3 * aceleracao) + (coeficiente3 * massa3 * gravidade));
+            var forcaBC = ((massa3 * aceleracao) + (forcaAtrito3));
             var velocidadeFinal = aceleracao * tempo;
             var deslocamento = (aceleracao * (tempo * tempo)) / 2;
 
+            if (aceleracao>0) {
             if (!isNaN(tempo)) {
                 var resultado = document.getElementById("resultado");
                 resultado.innerHTML = "Aceleração para a direita de: " + aceleracao.toFixed(2) + " m/s²<br>Força de A em B: " + forcaBA.toFixed(2) + " N<br>Força de B em C: " + forcaBC.toFixed(2) + " N<br>Força de atrito Corpo 1: " + forcaAtrito1.toFixed(2) + " N<br>Força de atrito Corpo 2: " + forcaAtrito2.toFixed(2) + " N<br>Força de atrito Corpo 3: " + forcaAtrito3.toFixed(2) + " N<br>Valor mínimo para força aplicada para empurrar os blocos: " + forcaAtritoTotal.toFixed(2) + " N<br>Velocidade final: " + velocidadeFinal.toFixed(2) + " m/s<br>Deslocamento: " + deslocamento.toFixed(2) + " m";
@@ -34,6 +33,11 @@ function calcular() {
                 var resultado = document.getElementById("resultado");
                 resultado.innerHTML = "Aceleração para a direita de: " + aceleracao.toFixed(2) + " m/s²<br>Força de A em B: " + forcaBA.toFixed(2) + " N<br>Força de B em C: " + forcaBC.toFixed(2) + " N<br>Força de atrito Corpo 1: " + forcaAtrito1.toFixed(2) + " N<br>Força de atrito Corpo 2: " + forcaAtrito2.toFixed(2) + " N<br>Força de atrito Corpo 3: " + forcaAtrito3.toFixed(2) + " N<br>Valor mínimo para força aplicada para empurrar os blocos: " + forcaAtritoTotal.toFixed(2) + " N";
             }
+        }
+        else {
+            var resultado = document.getElementById("resultado");
+                resultado.innerHTML = "Aceleração: " + aceleracao.toFixed(2) + " m/s²<br>Força de A em B: " + forcaBA.toFixed(2) + " N<br>Força de B em C: " + forcaBC.toFixed(2) + " N<br>Força de atrito Corpo 1: " + forcaAtrito1.toFixed(2) + " N<br>Força de atrito Corpo 2: " + forcaAtrito2.toFixed(2) + " N<br>Força de atrito Corpo 3: " + forcaAtrito3.toFixed(2) + " N<br>Valor mínimo para força aplicada para empurrar os blocos: " + forcaAtritoTotal.toFixed(2) + " N";
+        }
         } else {
             var resultado = document.getElementById("resultado");
             resultado.innerHTML = "A força é menor que a força de atrito total, logo o movimento é impossivel.<br>Valor mínimo para força aplicada para empurrar os blocos: " + forcaAtritoTotal.toFixed(2) + " N";
